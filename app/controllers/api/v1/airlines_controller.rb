500b61c59ffdb19ba475ce30c341d5684ec723ee
Chris Asmar <asmarcd@gmail.com>
2021-01-07 11:05:57 -0800

    routes all tested successfully

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,52 +1,55 @@
 module Api
     module V1
-        class AirlinesCOntroller < ApplicationController
+        class AirlinesController < ApplicationController
+            protect_from_forgery with: :null_session
+        
             def index
                 airlines = Airline.all
-                render json: AirlineSerializer.new(airlines, options).serialized_json
+                render json: AirlineSerializer.new(airlines).serialized_json
             end
 
             def show
-                airline = Airline.find_by(slug:params[slug])
-                render json: AirlineSerializer.new(airline, options).serialized_json
+                airline = Airline.find_by(slug: params[:slug])
+                render json: AirlineSerializer.new(airline).serialized_json
             end
 
             def create
                 airline = Airline.new(airline_params)
-            
+
                 if airline.save
                     render json: AirlineSerializer.new(airline).serialized_json
-                else 
-                    render json: {error: airline.errors.messages }, stats: 422
+                else
+                    render json: { error: airline.errors.messages }, status: 422
+                end
             end
-            
+
             def update
-                airline = Airline.find_by(slug:params[slug])
-            
+                airline = Airline.find_by(slug: params[:slug])
+
                 if airline.update(airline_params)
-                    render json: AirlineSerializer.new(airline, options).serialized_json
-                else 
-                    render json: {error: airline.errors.messages }, stats: 422
+                    render json: AirlineSerializer.new(airline).serialized_json
+                else
+                    render json: { error: airline.errors.messages }, status: 422
+                end
             end
 
             def destroy
-                airline = Airline.find_by(slug:params[slug])
-            
+                airline = Airline.find_by(slug: params[:slug])
+
                 if airline.destroy
-                    render head :no_content
-                else 
-                    render json: {error: airline.errors.messages }, stats: 422
+                    head :no_content
+                else
+                    render json: { error: airline.errors.messages }, status: 422
+                end
             end
 
             private
 
-            def airlines_params
+            def airline_params
                 params.require(:airline).permit(:name, :image_url)
             end
-
-            def options
-                @options ||= {inculde: %i:[reviews]}
-            end
+    
         end
     end
 end
+    
