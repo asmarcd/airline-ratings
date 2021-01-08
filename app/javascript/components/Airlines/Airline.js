4cf7f37edffd15059a5635901657f5f314d7d013
Chris Asmar <asmarcd@gmail.com>
2021-01-07 16:02:21 -0800

    styling for homepage complete

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    border: 1px solid #efefef;
+    background: #fff;
+    text-align: center;
+`
+const AirlineLogo = styled.div`
+    width: 50px;
+    text-align:center;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 10px;
+
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid #efefef;
+    }
+`
+const AirlineName = styled.div`
+    padding: 20px 0 10ps 0;
+`
+const LinkWrapper = styled.div`
+    margin: 30px 0 20px 0;
+    height: 50px;
+
+    a {
+        color: #fff;
+        background: #000;
+        border-radius: 4px;
+        padding: 10px 50px;
+        border: 1px solid #000;
+        width: 100%;
+        text-decoration: none;
+    }
+`
 
 const Airline = (props) => {
     return (
-        <div className = "card">
-            <section className = "airline-logo">
+        <Card>
+            <AirlineLogo>
                 <img src={props.attributes.image_url} alt ={props.attributes.name}/>
-            </section>
-            <section className = "airline-name">{props.attributes.name}</section>
+            </AirlineLogo>
+            <AirlineName>{props.attributes.name}</AirlineName>
             <section className = "airline-score">{props.attributes.avg_score}</section>
-            <section className = "airline-link">
+            <LinkWrapper>
                 <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
-            </section>
-        </div>
+            </LinkWrapper>
+        </Card>
     )
 }
 
